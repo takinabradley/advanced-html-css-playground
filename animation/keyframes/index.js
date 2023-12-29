e9241d98fe7f16d5c823ad5b43a392a20267b9a5
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2023-12-29 08:50:51 -0500

    Fix overflow issue with sliding text animation

--- animation/keyframes/index.js
+++ animation/keyframes/index.js
@@ -1,3 +1,3 @@
 /* animations can be enhanced using animation events in JavaScript */
-const slidingText = document.querySelector('.sliding-text')
+const slidingText = document.querySelector('.sliding-text__text')
 slidingText.addEventListener('animationend', e => slidingText.classList.add('sliding-text--white'))
